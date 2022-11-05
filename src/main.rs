4c2e1b01741da418011657882ce77c0c577f7fd4
Lexx Damm <lexx.damm@gmail.com>
2022-11-05 09:47:00 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process::exit;
+use invivofire::Reader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        exit(-1)
+    }
+    let command = &args[1];
+    match command.as_str() {
+        "info" => println!("Running info command"),
+        _ => {
+            println!("Can't recognize command");
+            exit(-1);
+        }
+    }
+    let mut nfc_reader = Reader::new();
+    nfc_reader.init();
+    exit(0);
 }
